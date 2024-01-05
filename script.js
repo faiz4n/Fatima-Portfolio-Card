27b26204ec064ea872e2b2742cce1ec6282a0919
Faizan <149582509+faiz4n@users.noreply.github.com>
2024-01-05 19:35:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function insta(){
+    alert("Bhai uske paas insta chalane ka time nhi hai");
+}
+
+function snap(){
+    alert("Snap bhi nhi chalati wo");
+}
+
+function wp(){
+    alert("Whatsapp chahiye tujhe? Naughty hora");
+}
+
+function ln(){
+    alert("will be updated soon");
+}
